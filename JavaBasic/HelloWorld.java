fffd0c7952cfb93b724cdbf82db116c7416369df
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-04-04 23:07:00 +0530

    Printing hello world in java

---
+++ JavaBasic/HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld{
+    public static void main(String args[]){
+        //println provides a new line and it is as same as \n
+        //by using continuos print statement it will print in the same line 
+        System.out.println("Hello World");
+        System.out.print("Hello world\n");
+        System.out.print("Hello World\nJava\n");
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+    }
+}
