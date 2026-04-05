57431960ec796d69793ae4d9d1b6563f9d8f947e
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-04-05 21:54:38 +0530

    Variable in java

---
+++ JavaBasic/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String [] args){
+        int a = 10;
+        int b = 20;
+        String name = "Ironman";
+        double c = 28.9;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(name);
+    }
+}
