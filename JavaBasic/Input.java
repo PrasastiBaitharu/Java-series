9a3abbea623f16feab9d35a01133b516eb7e8a5e
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-04-06 22:36:09 +0530

    Taking user input using Scanner class in java

---
+++ JavaBasic/Input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x;
+        System.out.print("Enter a number : ");
+        x = sc.nextInt();
+        System.out.println(x);
+        sc.close();
+    }
+}
