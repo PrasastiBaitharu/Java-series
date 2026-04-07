75f0076776dcc719b0ca059461552e31acd73ce3
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-04-07 23:25:50 +0530

    If else in java

---
+++ JavaBasic/Conditional_Statement/IfElse.java
@@ -0,0 +1,17 @@
+package conditionalStatement;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age : ");
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.println("Adult");
+        }else{
+            System.out.println("Teenager");
+        }
+        sc.close();
+    }
+}
