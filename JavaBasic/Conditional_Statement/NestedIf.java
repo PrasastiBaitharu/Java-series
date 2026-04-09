a1a60019f1e074eeb1d71c6e9065f25066b0aed2
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-04-09 21:46:05 +0530

    Nested if in java

---
+++ JavaBasic/Conditional_Statement/NestedIf.java
@@ -0,0 +1,25 @@
+package conditionalStatement;
+
+import java.util.Scanner;
+
+public class NestedIf {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int a , b;
+        System.out.print("Enter First Number : ");
+        a = sc.nextInt();
+        System.out.print("Enter Second Number");
+        b = sc.nextInt();
+
+        if(a == b){
+            System.out.print(a + " & " + b + " both are equal");
+        }else{
+            if(a>b){
+                System.out.println(a + " is greater");
+            }else{
+                System.out.println(b + " is greater");
+            }
+        }
+        sc.close();
+    }
+}
