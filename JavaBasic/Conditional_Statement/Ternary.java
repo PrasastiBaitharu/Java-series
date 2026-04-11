685e7ce08f692e2340d3948c0d393a64c8ee5e5d
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-04-11 22:27:22 +0530

    Ternary operator for conditions in java

---
+++ JavaBasic/Conditional_Statement/Ternary.java
@@ -0,0 +1,8 @@
+package conditionalStatement;
+
+public class Ternary {
+    public static void main(String [] args){
+        int a = 10;
+        System.out.println(a<50?"True" : "False");
+    }
+}
