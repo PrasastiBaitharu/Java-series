b31f5e75390289dad601a0c4621abb6795099f3b
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-04-21 23:12:02 +0530

    Sum in java

---
+++ JavaBasic/operator/arithmetic/Sum.java
@@ -0,0 +1,9 @@
+package operator.arithmetic;
+public class Sum {
+    public static void main(String [] args){
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
