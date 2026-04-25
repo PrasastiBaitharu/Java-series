ee8bd56e2f0176246830d04401471cef788e51ba
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-04-25 22:39:03 +0530

    Division in java

---
+++ JavaBasic/operator/arithmetic/Div.java
@@ -0,0 +1,10 @@
+package operator.arithmetic;
+
+public class Div {
+    public static void main(String [] args){
+        int a = 50;
+        int b = 5;
+        int div = a/b;
+        System.out.println(div);
+    }
+}
