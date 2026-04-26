161b57c2a314240f5f74f69c4196c65cb2bcd97a
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-04-26 23:18:43 +0530

    substraction in java

---
+++ JavaBasic/operator/arithmetic/Diff.java
@@ -0,0 +1,10 @@
+package operator.arithmetic;
+
+public class Diff {
+    public static void main(String [] args){
+        int a = 20;
+        int b = 10;
+        int diff = a - b;
+        System.out.println(diff);
+    }
+}
