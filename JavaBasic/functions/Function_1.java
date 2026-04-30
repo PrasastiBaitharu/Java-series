ab6e3e21802084023620c3753af31f9a5c9309bc
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-04-30 23:03:05 +0530

    Function without parameter and without return type

---
+++ JavaBasic/functions/Function_1.java
@@ -0,0 +1,11 @@
+package functions;
+
+class Function_1{
+    // without parameter without return type
+    public static void display(){
+        System.out.println("Hello world");
+    }
+    public static void main(String[] args){
+        display();
+    }
+}
