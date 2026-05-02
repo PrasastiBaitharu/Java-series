e84f9e9c0393b1a3c7a5d7c427b9add6b0b43027
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2026-05-02 23:00:40 +0530

    With parameter and withour return type

---
+++ JavaBasic/functions/Function_2.java
@@ -0,0 +1,11 @@
+package functions;
+
+public class Function_2 {
+    //with parameter without return type
+    public static void sum(int a, int b){
+        System.out.println("Sum is : " + (a+b));
+    }
+    public static void main(String[]args){
+        sum(5,10);
+    }
+}
